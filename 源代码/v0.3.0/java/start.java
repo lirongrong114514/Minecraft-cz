eece2833d52f7dbdf921aac1f2f1c5cdccd07ff4
lirongrong <lirongrong202202@163.com>
2024-03-06 02:52:40 +0000

    ***
    Signed-off-by: lirongrong <lirongrong202202@163.com>

--- 源代码/v0.3.0/java/start.java
+++ 源代码/v0.3.0/java/start.java
@@ -6,14 +6,15 @@ public class start extends JFeram{
     public static void main(String[] args) {
         System.out.println("Minecraft正在初始化");
         int seed;
-        System.out.println("请问是否生成随机种子y/n?");
-        Scanner _q_ = new Scanner(System.in);
-        String __ass__ = _q_.nextLine();
-        if(__ass__.equals("n")) {
-            seed=def.input();
-        }else {
-            seed = def.seed(1000000000,2147483647);
-        }
+            System.out.println("请问是否生成随机种子y/n?");
+            Scanner _q_ = new Scanner(System.in);
+            String __ass__ = _q_.nextLine();
+            if(__ass__.equals("n")) {
+                try{
+                    seed=def.input();}
+            }else {
+                seed = def.seed(1000000000,2147483647);
+            }
         System.out.println("Minecraft初始化完毕正在生成地形");
         int[] arr=def.splitInteger(seed);
         String Generate = def.Generate(arr);
